62881548889a523c21f1d890679077e4fcd60ea4
Mark M <48392746+devlopuk@users.noreply.github.com>
2019-11-06 17:12:12 +0000

    Delete 01.js

--- 01.js
+++
@@ -1,5 +0,0 @@
-var message = prompt("Enter your message");
-var typedmsg = message.length;
-var length = 280 - typedmsg;
-
-alert("You have written " + message.length + " characters, you have " + length + " characters left.");
