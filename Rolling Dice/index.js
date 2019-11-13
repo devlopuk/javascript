5bc849a7d01c6c930bd0db55f2602b708fa0d121
Mark M <48392746+devlopuk@users.noreply.github.com>
2019-11-13 20:25:24 +0000

    Add files via upload

--- Rolling Dice/index.js
+++ Rolling Dice/index.js
@@ -1,96 +1,14 @@
-var buttonColours =["red", "blue", "green", "yellow"];
-  var gamePattern = [];
-  var userClickedPattern = [];
 
-  var started = false;
-  var level = 0;
+var rd1= Math.floor(Math.random() * 6)+1;
+var rd2= Math.floor(Math.random() * 6)+1;
 
-  $(".btn").click(function() {
+document.querySelector(".img1").setAttribute("src","images/dice"+rd1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+rd2+".png");
 
-  var userChosenColour = $(this).attr("id");
-
-userClickedPattern.push(userChosenColour);
-//console.log(userClickedPattern);
-playSound(userChosenColour);
-animatePress(userChosenColour);
-checkAnswer(userClickedPattern.length-1);
-});
-
-$(document).keydown(function(){
-
-if (!started){
-
-$("#level-title").text("level: "+level);
-nextSequence();
-started = true;
-
-}
-
-});
-
-
-
-function nextSequence() {
-userClickedPattern=[];
-level++;
-$("#level-title").text("level: "+level);
-  var randomNumber= Math.floor(Math.random()*4);
-  var randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-var audio = new Audio("sounds/"+randomChosenColour+".mp3");
-audio.play();
-}
-
-function playSound(name){
-  var audio = new Audio("sounds/"+name+".mp3");
-  audio.play();
-
-}
-
-function animatePress(currentColour){
-  $("#"+currentColour).addClass("pressed");
-
-  setTimeout(function(){
-      $("#"+currentColour).removeClass("pressed");
-  },100);
-}
-
-
-function checkAnswer (currentLevel){
-if (userClickedPattern[currentLevel]===gamePattern[currentLevel])
-{
-  console.log("Success");
-
-  if (userClickedPattern.length===gamePattern.length)
-  {
-    setTimeout(function () {
-
-      nextSequence();
-    },1000);
-  }
-
-
-}
-else
-{
-  console.log ("Wrong");
-  var audio = new Audio("sounds/wrong.mp3");
-  audio.play();
-  $("body").addClass("game-over");
-  setTimeout(function () {
-
-    $("body").removeClass("game-over");
-  },200);
-$("h1").text("Game Over, Press Any Key to Restart");
-startOver();
-}
-}
-
-function startOver(){
-level =0;
-gamePattern=[];
-started = false;
-
+if (rd1 > rd2) {
+document.querySelector("H1").textContent = 'Player 1 Wins!';
+} else if (rd1 ===  rd2) {
+document.querySelector("H1").textContent = "It's a Draw!";
+} else {
+document.querySelector("H1").textContent = 'Player 2 Wins!';
 }
